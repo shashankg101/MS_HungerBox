41b91f99db49d7627357fcf4b3174cad35efcd24
shashankg101 <shashankgowdas2003@gmail.com>
2024-05-30 15:49:46 +0530

    Update menu.module.ts

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AzureCosmosDbModule } from '@nestjs/azure-database';
+
+import { menu } from './menu.entity';
 import { MenuController } from './menu.controller';
-import { Menu1Controller } from './menu1.controller';
 
 @Module({
-  controllers: [MenuController,Menu1Controller]
+  imports: [
+    AzureCosmosDbModule.forFeature([{ collection: 'menu', dto:menu }])],
+  controllers: [MenuController],
 })
 export class MenuModule {}
