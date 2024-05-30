f0171d103d7e5988d0e18c0498a67da6572a510c
shashankg101 <shashankgowdas2003@gmail.com>
2024-05-30 15:52:34 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AzureCosmosDbModule } from '@nestjs/azure-database';
 import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [MenuModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AzureCosmosDbModule.forRoot({
+      dbName: 'MS_HungerBox',
+      endpoint: 'https://ms-hunger-box.documents.azure.com:443/',
+      key: 'V7mPnlWUZySjELD3z9VHsbj31ylLRmOw4A09moAFfRqsrADiLHzQQUMNT6KxM18dcLgDgXwC99x3ACDbYdzI0w==',
+    }),
+    MenuModule
+  ],
+  
 })
 export class AppModule {}
