64611abd3c4385a8e2925743df627bdaf1357274
Pratikhya <92398878+Satorusimpp@users.noreply.github.com>
2024-05-30 11:41:16 +0530

    Update menu.module.ts

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MenuController } from './menu.controller';
+import { Menu1Controller } from './menu1.controller';
 
 @Module({
-  controllers: [MenuController]
+  controllers: [MenuController,Menu1Controller]
 })
 export class MenuModule {}
