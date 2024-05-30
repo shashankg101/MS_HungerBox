31b6635fd4077ba9976a08dfe9e0fc87984110e5
shashankg101 <shashankgowdas2003@gmail.com>
2024-05-30 11:23:03 +0530

    Update menu.controller.ts

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -47,34 +47,4 @@ export class MenuController {
       price: resource.price,
     };
   }
-
-  //Update an item in the database
-  @Put('update')
-  async update(@Body() payload: MenuDto) {
-    const itemupdate = new menu();
-    itemupdate.id = payload.id;
-    itemupdate.outlet_name = payload.outlet_name;
-    itemupdate.item_name = payload.item_name;
-    itemupdate.calories = payload.calories;
-    itemupdate.price = payload.price;
-
-    const { resource } = await this.menuContainer.items.upsert(itemupdate);
-    return {
-      id: resource.id,
-      outlet_name: resource.outlet_name,
-      item_name: resource.item_name,
-      calories: resource.calories,
-      price: resource.price,
-    };
-  }
-
-  //Delete an item from the database
-  @Delete('remove')
-  async remove(
-    @Query('id') id: string,
-    @Query('partitionkey') partitionkey: string,
-  ) {
-    await this.menuContainer.item(id, partitionkey).delete();
-    return 'deleted';
-  }
 }
