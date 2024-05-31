e7c1fea49e85ba4c26138aadc4f5b9f6889d2866
Shashank Gowda <shashankgowdaS2003@gmail.com>
2024-05-31 10:22:19 +0530

    Readded all uodated files

---
+++ src/menu/menu1.controller.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { Menu1Controller } from './menu1.controller';
+
+describe('Menu1Controller', () => {
+  let controller: Menu1Controller;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [Menu1Controller],
+    }).compile();
+
+    controller = module.get<Menu1Controller>(Menu1Controller);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
