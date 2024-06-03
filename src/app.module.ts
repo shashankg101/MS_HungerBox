b8a31526b89e098d92a3999fb855a948121041d6
Pratikhya <yopratikhyadhar31@gmail.com>
2024-06-03 14:16:47 +0530

    test commit 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { CorrelationIdMiddleware } from './correlation/correlation-id.middleware
       endpoint: 'https://ms-hunger-box.documents.azure.com:443/',
       key: 'V7mPnlWUZySjELD3z9VHsbj31ylLRmOw4A09moAFfRqsrADiLHzQQUMNT6KxM18dcLgDgXwC99x3ACDbYdzI0w==',
     }),
-    MenuModule,
+    MenuModule, //menumodule
   ],
   providers: [CorrelationIdService],
 })
